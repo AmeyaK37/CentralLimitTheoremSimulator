affce9c89a38d88c5ce0d0c9e6e1a5e4a9e386e2
AmeyaK37 <118205506+AmeyaK37@users.noreply.github.com>
2023-11-30 10:39:52 -0600

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -27,34 +27,106 @@ document.addEventListener("DOMContentLoaded", () => {
   });
  
   // Generate data from various distributions
-  function generatePopulationData(distribution) {
-    // Implement data generation based on the chosen distribution
-    // Return an array of data points
-  }
- 
-  // Generate sampling distribution of sample means
-  function generateSamplingDistribution(populationData, sampleSize) {
-    // Implement sampling distribution generation using the given sample size
-    // Return an array of sample means
-  }
- 
-  // Plot a bar chart on the canvas
-  function plotBarChart(canvas, data, title) {
-    // Implement code to plot a bar chart on the canvas using the provided data
-  }
- 
-  // Plot a line chart on the canvas
-  function plotLineChart(canvas, data, title) {
-    // Implement code to plot a line chart on the canvas using the provided data
-  }
- 
-  // Calculate mean of an array
-  function calculateMean(data) {
-    // Implement mean calculation
-  }
- 
-  // Calculate standard deviation of an array
-  function calculateStdDev(data) {
-    // Implement standard deviation calculation
-  }
+function generatePopulationData(distribution) {
+  // Implement data generation based on the chosen distribution
+  // Return an array of data points
+  // Example for generating random data for the Normal distribution:
+  const data = [];
+  if (distribution === 'normal') {
+    for (let i = 0; i < 1000; i++) {
+      data.push(Math.random() * 100); // Generate random numbers for a normal distribution
+    }
+  } else if (distribution === 'uniform') {
+    // Generate data for a uniform distribution
+  } else if (distribution === 'bimodal') {
+    // Generate data for a bimodal distribution
+  }
+  return data;
+}
+
+// Generate sampling distribution of sample means
+function generateSamplingDistribution(populationData, sampleSize) {
+  // Implement sampling distribution generation using the given sample size
+  // Return an array of sample means
+  const samplingDistribution = [];
+  for (let i = 0; i < 1000; i++) {
+    const sample = [];
+    for (let j = 0; j < sampleSize; j++) {
+      const randomIndex = Math.floor(Math.random() * populationData.length);
+      sample.push(populationData[randomIndex]);
+    }
+    const sampleMean = calculateMean(sample);
+    samplingDistribution.push(sampleMean);
+  }
+  return samplingDistribution;
+}
+
+// Plot a bar chart on the canvas
+function plotBarChart(canvas, data, title) {
+  // Implement code to plot a bar chart on the canvas using the provided data
+  // Example implementation using Chart.js library:
+  const ctx = canvas.getContext("2d");
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: [...Array(data.length).keys()],
+      datasets: [{
+        label: title,
+        data: data,
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+
+// Plot a line chart on the canvas
+function plotLineChart(canvas, data, title) {
+  // Implement code to plot a line chart on the canvas using the provided data
+  // Example implementation using Chart.js library:
+  const ctx = canvas.getContext("2d");
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [...Array(data.length).keys()],
+      datasets: [{
+        label: title,
+        data: data,
+        fill: false,
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+
+// Calculate mean of an array
+function calculateMean(data) {
+  // Implement mean calculation
+  return data.reduce((acc, val) => acc + val, 0) / data.length;
+}
+
+// Calculate standard deviation of an array
+function calculateStdDev(data) {
+  // Implement standard deviation calculation
+  const mean = calculateMean(data);
+  const variance = data.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0) / data.length;
+  return Math.sqrt(variance);
+}
+
 });
